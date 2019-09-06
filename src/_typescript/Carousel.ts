3a33e97ff87293e817059549195bff8ac258062e
Tyler Scott <tyduptyler13@gmail.com>
2019-09-06 11:22:07 -0600

    Carousel changes and font awesome pro update.

--- src/_typescript/Carousel.ts
+++ src/_typescript/Carousel.ts
@@ -2,9 +2,11 @@ import ClickEvent = JQuery.ClickEvent;
 
 export class Carousel {
   platform: JQuery;
+  width: number;
 
   constructor(element: JQuery) {
     this.platform = element.find(".fusion-go-round-platform");
+    this.width = element.find('.fusion-go-round-car:first').outerWidth();
     element.find(".fusion-go-round-left")
         .on('click', this.handleLeft.bind(this));
     element.find(".fusion-go-round-right")
@@ -14,14 +16,14 @@ export class Carousel {
   private handleLeft(event: ClickEvent) {
     event.preventDefault();
     this.platform.finish().animate({
-      scrollLeft: Math.max(this.platform.scrollLeft() - 282, 0)
+      scrollLeft: Math.max(this.platform.scrollLeft() - this.width, 0)
     }, 500);
   }
 
   private handleRight(event: ClickEvent) {
     event.preventDefault();
     this.platform.finish().animate({
-      scrollLeft: this.platform.scrollLeft() + 282
+      scrollLeft: this.platform.scrollLeft() + this.width
     }, 500);
   }
 }
